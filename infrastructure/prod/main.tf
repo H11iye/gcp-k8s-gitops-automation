5c60c90b57106b386887c8791c7a38ee872ffbe7
H11iye <hamdi.iyed11@gmail.com>
2025-10-14 14:12:50 +0000

    Define main

---
+++ infrastructure/prod/main.tf
@@ -0,0 +1,21 @@
+module "gke" {
+  source = "../modules/gke"
+    project_id = var.project_id
+    region     = var.region
+    cluster_name = "gke-${terraform.workspace}"
+}
+
+module "storage" {
+  source = "../modules/storage"
+  buckert_name = "input-bucket-${terraform.workspace}"
+}
+
+module "cloud_function" {
+  source = "../modules/cloud-function"
+  bucket_trigger = module.storage.buckert_name
+}
+
+module "argocd" {
+  source = "../modules/argocd"
+    cluster_name = module.gke.cluster_name
+}
